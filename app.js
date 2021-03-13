6e4a8c500c16f6328df4325eab32967946171a82
tumee <tmkee0525@gmail.com>
2021-03-13 20:18:28 +0900

    js suuliin nemelt

---
+++ app.js
@@ -0,0 +1,49 @@
+//delegtste ajilldag controller
+
+var uilcontroller=(function(){
+
+})();
+
+//sankhuute ajilah controller
+var financeController=(function(){
+
+})();
+
+//progrmaiin controller
+var appcontroller=(function(uilcontroller,financeController){
+document.querySelector(".add__btn").addEventListener("click",function(){
+//oruulah ugugdliig delgetsees avna
+
+
+// olj avsan ugugdluudee controllert damjuulna 
+
+
+//olj avsan ugugluudee wbe deeree tohiroh herest tavine 
+
+
+//tusuviig toootsono
+
+//etssin uldegdel toootsoog delgetsen gargana
+
+});
+document.addEventListener('keypress',function(event){
+console.log(event.keycode)
+// if(event.key==="enter")
+// {
+//     console.log("sdsa")
+    
+// }
+// else
+// {
+//     console.log("nono")
+// }
+    // if(event.keycode==13) {
+//     console.log("enter bn")
+    
+// }
+// else
+// {
+//     console.log("enterneed busad")
+// }
+})
+})(uilcontroller,financeController);
